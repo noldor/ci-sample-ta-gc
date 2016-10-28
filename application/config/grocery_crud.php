d610a07416205693f0e0acebad762b575c321de4
TAKEKOSHI Akishige <akishige@takekoshi.net>
2016-10-28 20:46:28 +0900

    日本語対応

--- application/config/grocery_crud.php
+++ application/config/grocery_crud.php
@@ -2,10 +2,10 @@
     exit('No direct script access allowed');
 }
     // For view all the languages go to the folder assets/grocery_crud/languages/
-    $config['grocery_crud_default_language'] = 'english';
+    $config['grocery_crud_default_language'] = 'japanese';
 
     // There are only three choices: "uk-date" (dd/mm/yyyy), "us-date" (mm/dd/yyyy) or "sql-date" (yyyy-mm-dd)
-    $config['grocery_crud_date_format'] = 'uk-date';
+    $config['grocery_crud_date_format'] = 'sql-date';
 
     // The default per page when a user firstly see a list page
     $config['grocery_crud_default_per_page'] = 10;
