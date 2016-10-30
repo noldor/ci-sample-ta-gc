b5de1a36880b3df232fe015e2b119670c5b4645d
TAKEKOSHI Akishige <akishige@takekoshi.net>
2016-10-30 15:57:31 +0900

    使用していない機能を削除

--- application/models/tables/Users_table.php
+++ application/models/tables/Users_table.php
@@ -4,8 +4,35 @@ require_once APPPATH . 'libraries/MY_Grocery_crud_model.php';
 
 class Users_table extends MY_Grocery_crud_model
 {
-    protected $record_created_at = false;
-    protected $record_updated_at = false;
-    protected $record_created_by = false;
-    protected $record_updated_by = false;
+    public function db_insert($post_array)
+    {
+        $ci = get_instance();
+        if ($this->record_created_at) {
+            $post_array['created_at'] = $this->clock->format('Y-m-d H:i:s');
+        }
+        if ($this->record_created_by) {
+            $post_array['created_by'] = isset($ci->auth) ? $ci->auth->get_username() : '';
+        }
+        if ($this->record_updated_at) {
+            $post_array['updated_at'] = $this->clock->format('Y-m-d H:i:s');
+        }
+        if ($this->record_updated_by) {
+            $post_array['updated_by'] = isset($ci->auth) ? $ci->auth->get_username() : '';
+        }
+
+        return parent::db_insert($post_array);
+    }
+
+    public function db_update($post_array, $primary_key_value)
+    {
+        $ci = get_instance();
+        if ($this->record_updated_at) {
+            $post_array['updated_at'] = $this->clock->format('Y-m-d H:i:s');
+        }
+        if ($this->record_updated_by) {
+            $post_array['updated_by'] = isset($ci->auth) ? $ci->auth->get_username() : '';
+        }
+
+        return parent::db_update($post_array, $primary_key_value);
+    }
 }
