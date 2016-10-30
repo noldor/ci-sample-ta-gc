ea80d79d6a001ef2596bde1ae3c98bddbe562d68
TAKEKOSHI Akishige <akishige@takekoshi.net>
2016-10-30 17:26:27 +0900

    カラム表示調整、パスワード入力時のハッシュ

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,11 +2,11 @@
 
 class User extends MY_Controller
 {
-    public function crud()
+    public function crud($action = null)
     {
         $validation_rules = array(
             ['field' => 'id',                     'label' => 'ID',                           'rules' => ''],
-            ['field' => 'password',               'label' => 'パスワード',                   'rules' => ''],
+            ['field' => 'password_raw',           'label' => 'パスワード',                   'rules' => ''],
             ['field' => 'email',                  'label' => 'メールアドレス',               'rules' => 'trim|required'],
             ['field' => 'activated',              'label' => '有効状態',                     'rules' => 'in_list[0,1]|required'],
             ['field' => 'banned',                 'label' => 'banフラグ',                    'rules' => 'in_list[0,1]|required'],
@@ -26,16 +26,26 @@ class User extends MY_Controller
         $this->crud->set_table('users')
                    ->set_subject('ユーザ')
                    ->columns('id', 'email', 'activated', 'banned', 'last_login', 'created')
-                   ->fields('password', 'email', 'activated', 'banned', 'ban_reason', 'new_password_key',
+                   ->fields('password_raw', 'email', 'activated', 'banned', 'ban_reason', 'new_password_key',
                             'new_password_requested', 'new_email', 'new_email_key', 'last_ip', 'last_login',
                             'created', 'modified')
+                   ->field_type('password_raw', 'password')
                    ->field_type('ban_reason', 'text')
                    ->unset_texteditor('ban_reason')
+                   ->field_type('last_ip', 'readonly')
+                   ->field_type('last_login', 'readonly')
+                   ->field_type('created', 'readonly')
+                   ->field_type('modified', 'readonly')
                    ;
         foreach ($validation_rules as $rule) {
             $this->crud->display_as($rule['field'], $rule['label']);
         }
         $this->crud->set_rules($validation_rules);
+
+        // 新規作成時のみパスワードを必須にする
+        if ($action == 'insert_validation' || $action == 'insert') {
+            $this->crud->set_rules('password_raw', 'パスワード', 'required');
+        }
 
         $this->load->view('user/crud.php', $this->crud->render());
     }
--- application/models/tables/Users_table.php
+++ application/models/tables/Users_table.php
@@ -6,33 +6,32 @@ class Users_table extends MY_Grocery_crud_model
 {
     public function db_insert($post_array)
     {
-        $ci = get_instance();
-        if ($this->record_created_at) {
-            $post_array['created_at'] = $this->clock->format('Y-m-d H:i:s');
-        }
-        if ($this->record_created_by) {
-            $post_array['created_by'] = isset($ci->auth) ? $ci->auth->get_username() : '';
-        }
-        if ($this->record_updated_at) {
-            $post_array['updated_at'] = $this->clock->format('Y-m-d H:i:s');
-        }
-        if ($this->record_updated_by) {
-            $post_array['updated_by'] = isset($ci->auth) ? $ci->auth->get_username() : '';
-        }
+        $post_array['created'] = $this->clock->format('Y-m-d H:i:s');
+        $post_array['modified'] = $this->clock->format('Y-m-d H:i:s');
+
+        $post_array['password'] = $this->hash_password($post_array['password_raw']);
+        unset($post_array['password_raw']);
 
         return parent::db_insert($post_array);
     }
 
     public function db_update($post_array, $primary_key_value)
     {
-        $ci = get_instance();
-        if ($this->record_updated_at) {
-            $post_array['updated_at'] = $this->clock->format('Y-m-d H:i:s');
+        $post_array['modified'] = $this->clock->format('Y-m-d H:i:s');
+
+        if (strlen($post_array['password_raw']) > 0) {
+            $post_array['password'] = $this->hash_password($post_array['password_raw']);
         }
-        if ($this->record_updated_by) {
-            $post_array['updated_by'] = isset($ci->auth) ? $ci->auth->get_username() : '';
-        }
+        unset($post_array['password_raw']);
 
         return parent::db_update($post_array, $primary_key_value);
     }
+
+    private function hash_password($password)
+    {
+        $hasher = new PasswordHash(
+                $this->config->item('phpass_hash_strength', 'tank_auth'),
+                $this->config->item('phpass_hash_portable', 'tank_auth'));
+        return $hasher->HashPassword($password);
+   }
 }
